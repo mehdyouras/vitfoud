f54404fafd2f5b723f6d0ccedbecd5c41bbbfd79
leny <info@flatland.be>
2017-04-22 22:13:16 +0200

    Setup tooling

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+/* hepl-dw/vitfoud-client
+ *
+ * /gulpfile.js - Gulp tasks
+ *
+ * coded by leny@flatLand!
+ * started at 22/04/2017
+ */
+
+var gulp = require( "gulp" ),
+    pug = require( "gulp-pug" ),
+    sass = require( "gulp-sass" ),
+    autoprefixer = require( "gulp-autoprefixer" ),
+    csso = require( "gulp-csso" ),
+    browserify = require( "browserify" ),
+    source = require( "vinyl-source-stream" ),
+    uglify = require( "gulp-uglify" ),
+    buffer = require( "vinyl-buffer" ),
+    babelify = require( "babelify" );
+
+var aLibs = Object.keys( require( "./package.json" ).dependencies );
+
+// --- Task for pug
+
+gulp.task( "html", function() {
+    gulp.src( "src/pug/**/*.pug" )
+        .pipe( pug( {} ) )
+        .pipe( gulp.dest( "." ) );
+} );
+
+// --- Task for styles
+
+gulp.task( "css", function() {
+    gulp.src( "src/sass/**/*.scss" )
+        .pipe( sass().on( "error", sass.logError ) )
+        .pipe( autoprefixer() )
+        .pipe( csso() )
+        .pipe( gulp.dest( "assets/css" ) );
+} );
+
+// --- Tasks for js
+
+gulp.task( "js-libs", function() {
+    // process.env.NODE_ENV = "production";
+    browserify()
+        .require( aLibs )
+        .bundle()
+        .pipe( source( "libs.min.js" ) )
+        .pipe( buffer() )
+        .pipe( uglify() )
+        .pipe( gulp.dest( "assets/js" ) );
+} );
+
+gulp.task( "js-app", function() {
+    browserify( "src/js/main.js" )
+        .transform( babelify )
+        .external( aLibs )
+        .bundle()
+        .pipe( source( "app.min.js" ) )
+        .pipe( buffer() )
+        .pipe( uglify() )
+        .pipe( gulp.dest( "assets/js" ) );
+} );
+
+gulp.task( "js", [ "js-libs", "js-app" ] );
+
+// --- Watch tasks
+
+gulp.task( "watch", function() {
+    gulp.watch( "src/pug/**/*.pug", [ "html" ] );
+    gulp.watch( "src/sass/**/*.scss", [ "css" ] );
+    gulp.watch( "src/js/**/*.js", [ "js-app" ] );
+} );
+
+// --- Aliases
+
+gulp.task( "default", [ "html", "css", "js" ] );
+gulp.task( "work", [ "default", "watch" ] );
---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+/* hepl-dw/vitfoud-client
+ *
+ * /src/js/main.js - Entry point for vitfoud-client
+ *
+ * coded by leny@flatLand!
+ * started at 22/04/2017
+ */
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <h1>{ "Vitfoud, in React!" }</h1>,
+    document.querySelector( "#app" )
+);
