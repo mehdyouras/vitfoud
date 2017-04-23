1b1eeff34f81406017dd5caa4e0960bbd72b048e
leny <info@flatland.be>
2017-04-23 16:27:48 +0200

    Add util function to convert decimal hour as readable string

--- src/js/utils/misc.js
+++ src/js/utils/misc.js
@@ -10,6 +10,17 @@ export const hoursAsDecimal = function( iHours, iMinutes ) {
     return iHours + ( iMinutes / 60 );
 };
 
+export const hoursAsString = function( iDecimalHours ) {
+    let sHours = Math.round( iDecimalHours ),
+        sMinutes = ( iDecimalHours % 1 ) * 60;
+
+    ( sHours === 24 ) && ( sHours = 0 );
+    ( sHours < 10 ) && ( sHours = `0${ sHours }` );
+    ( sMinutes < 10 ) && ( sMinutes = `0${ sMinutes }` );
+
+    return `${ sHours }:${ sMinutes }`;
+};
+
 export const isOpenWithHours = function( aHours ) {
     let dNow = new Date(),
         iNow = hoursAsDecimal( dNow.getHours(), dNow.getMinutes() ),
